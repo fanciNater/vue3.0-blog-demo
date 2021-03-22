9e81672bb785052b297cc64b16bd90b93be7a6d7
吴灿 <wucan@wucandeMacBook-Pro.local>
2021-03-22 16:41:52 +0800

    feat(打包): package dev

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,24 +1,25 @@
 <!--
  * @Author: your name
  * @Date: 2021-03-15 15:38:11
- * @LastEditTime: 2021-03-15 17:53:09
+ * @LastEditTime: 2021-03-22 16:41:15
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /vsCodeProjects/demo/vue-study/blogs-admin/src/views/Home.vue
 -->
 <template>
   <div class="home">
-    <page-header></page-header>
+    demo
+    <!-- <page-header></page-header> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src'
-import PageHeader from "@/components/header";
+// import PageHeader from "@/components/header";
 export default {
   name: "Home",
   components: {
-    PageHeader
+    // PageHeader
   }
 };
 </script>
